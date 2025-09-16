05af98937fe14c84c285b82f921cbf45711c70ff
mohammedip <reidamohammed3@gmail.com>
2025-09-16 10:33:03 +0100

    delete python files

--- challenge1.py
+++
@@ -1,8 +0,0 @@
-print("Enter your name")
-name = input()
-
-print("Enter your age")
-age = input()
-
-
-print("hello " + name + " you are "+age+" years old")
--- challenge5.py
+++
@@ -1,9 +0,0 @@
-N =  int(input("enter your number : "))
-
-i=1
-result=0
-while(i<=N):
-    result +=i
-    i+=1
-
-print(f"your result {result}")    
